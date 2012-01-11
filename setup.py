d3a6139880065b079e75c3cafe65184b62767fcc
Roman Bogorodskiy <bogorodskiy@gmail.com>
2012-01-12 01:26:29 +0400

    Add deps to setup.py, packages and bump version.

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 
 setup(
         name = "yaslov",
-        version = "0.2",
+        version = "0.3",
         description="CLI client for slovari.yandex.ru",
         author = "Roman Bogorodskiy",
         author_email = "bogorodskiy@gmail.com",
+        packages = ['yasl'],
         scripts = ["yaslov"],
-        classifiers=[
+        requires = ["pyxdg"],
+        classifiers = [
             'Development Status :: 5 - Production/Stable',
             'Environment :: Console',
             'Intended Audience :: End Users/Desktop',
