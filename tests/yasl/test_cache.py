302e38baeae4a212680e68fd779c28c7bccac181
Roman Bogorodskiy <bogorodskiy@gmail.com>
2012-01-12 09:18:12 +0400

    Add tests for Cache: _init_db(), find() and update().

--- tests/yasl/test_cache.py
+++ tests/yasl/test_cache.py
@@ -47,3 +47,55 @@ class TestCache(object):
 
         mock_os_path.isdir.assert_called_with(fake_dirname)
         assert_false(mock_os_makedirs.called)
+
+    @patch.object(Cache, '_init_paths')
+    def test__init_db_creates_all_the_tables(self, mock_cache_init_paths):
+        expected_tables = set([u'requests', u'timestamp'])
+        mock_cache_init_paths.return_value = ":memory:"
+
+        cache = Cache()
+
+        cache._cur.execute("""SELECT name FROM sqlite_master
+                WHERE type='table'""")
+        actual_tables = set([i[0] for i in cache._cur.fetchall()])
+        assert_equal(actual_tables, expected_tables)
+
+    @patch.object(Cache, '_init_paths')
+    def test_add_and_find(self, mock_cache_init_paths):
+        mock_cache_init_paths.return_value = ":memory:"
+
+        cache = Cache()
+        
+        request = "request"
+        response = "sample response"
+        cache.add(request, response)
+
+        cache._cur.execute("""SELECT * FROM requests
+            WHERE request=? AND response=?""", (request, response))
+        assert_equals(len(cache._cur.fetchall()), 1)
+
+        assert_equals(cache.find(request), response)
+
+    @patch.object(Cache, '_init_paths')
+    def test_find_of_nonexist_request(self, mock_cache_init_paths):
+        mock_cache_init_paths.return_value = ":memory:"
+
+        cache = Cache()
+        assert_equals(cache.find("nonexist"), None)        
+
+    @patch.object(Cache, '_init_paths')
+    def test_update(self, mock_cache_init_paths):
+        mock_cache_init_paths.return_value = ":memory:"
+
+        cache = Cache()
+
+        request = response = "foo"
+        cache.add(request, response)
+
+        for _ in range(0, 5):
+            cache.update(request)
+
+        cache._cur.execute("""SELECT t.timestamp FROM timestamp as t 
+            JOIN requests as r ON t.request_id = r.id
+            WHERE r.request=?""", (request,))
+        assert_equals(len(cache._cur.fetchall()), 5)
